848da5ba4089a87f01cc9d3ca41b15e3b265c80a
RajivNkomoElla <rajiv.nkomo@ez-audiovisuel.com>
2025-05-01 23:14:25 +0100

    correction de CustomUser.__str__

--- accounts/models.py
+++ accounts/models.py
@@ -5,5 +5,6 @@ class CustomUser(AbstractUser):
     is_admin = models.BooleanField(default=False)
     
     def __str__(self):
-        return f"{self.username} { "(admin)" if self.is_admin  else ""}"
+        admin = '(admin)' if self.is_admin  else ""
+        return f"{self.username} {admin}"
     
